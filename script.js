02483c6f302706a64e9413e5c267670442ab4051
Mohdjamal1 <mohdjamalh786@gmail.com>
2023-04-15 14:34:56 +0530

    complete the function

--- script.js
+++ script.js
@@ -8,20 +8,44 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map((item) =>{
+    if (item.profession === "developer") {
+      console.log(item);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((value) =>{
+    if (value.profession === "developer") {
+      console.log(value);
+    }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+  let obj = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(obj);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  let removeArr = arr.filter((val) =>{
+    return val.profession !== "admin" 
+  });
+  console.log(removeArr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2 = [
+  {id: 4, name: "Mohd Jamal", age: "24", profession: "DataAnalyst"},
+  {id: 5, name: "Yuvraaj singh", age: "26", profession: "Programmer"},
+  {id: 6, name: "Vikaas", age: "23", profession:"Student"},
+];
+  let arr3 = arr.concat(arr2);
+  console.log(arr3);
 }
